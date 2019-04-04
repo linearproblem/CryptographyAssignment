7e65ce67c2fe532332e5ac3611b843988e396c21
linearproblem <c3261440@uon.edu.au>
2019-04-04 14:50:49 +1100

    Worked on menu system - Added Header - Added encryption/decryption menu options

--- main.c
+++ main.c
@@ -8,21 +8,32 @@ INCLUDE LIBRARIES
 /******************************************************************************
 DECLARE FUNCTIONS
 ******************************************************************************/
+void printHeader();
+void mainMenu();
 void helloWorld(); // Prints the text "Hello World into the console"
 void encryptString();
 // ADD ZERO ARRAYS
+
+void guessNum();
 
 
 /******************************************************************************
 MAIN
 ******************************************************************************/
 int main(){
-  // Runs hello world function
-    void helloWorld();
+   // guessNum();
 
-  // Runs text encrytion function
-    encryptString();
+    // Prints application header
+          printHeader();
+    // Opens user menu
+         mainMenu();
+    // Runs hello world function
+        helloWorld();
 
+    // Runs text encryption function
+        //encryptString();
+        int x;
+    scanf("%d",&x); // temp delay
     return 0;
 }
 
@@ -31,9 +42,81 @@ int main(){
 EXTERNAL FUNCTIONS
 ******************************************************************************/
 
+void printHeader(){
+    // Prints splash back
+    printf("+-----------------------------------------------------------------------------------+\n");
+    printf("|                                                                                   |\n");
+    printf("|     XXXXXXXXX  XXXXXXXXX  XXXXXXXXXXXXX    X         X  XXXXXXXXXXX  XXXXXXXXXXX  |\n");
+    printf("|   XXX              X      X           X    X         X  X            X         XX |\n");
+    printf("|  XX                X      X           XX   X         X  X            X          X |\n");
+    printf("| XX                 X      X           XX   X         X  X            X          X |\n");
+    printf("| X                  X      X           X    X         X  X            X          X |\n");
+    printf("| X                  X      XXXXXXXXXXXX     XXXXXXXXXXX  XXXXXXXX     XXXXXXXXXXXX |\n");
+    printf("| X                  X      XX               X         X  X            X   XX       |\n");
+    printf("| X                  X      X                X         X  X            X    XXX     |\n");
+    printf("| XX                 X      X                X         X  X            X      XXX   |\n");
+    printf("|  XXX               X      X                X         X  X            X        XX  |\n");
+    printf("|    XXXXXXXXXX  XXXXXXXXX  X                X         X  XXXXXXXXXXX  X         XX |\n");
+    printf("|                                                                                   |\n");
+    printf("+-----------------------------------------------------------------------------------+\n");
+}
+
+void mainMenu() {
+    // Run Hello World Function
+    printf("\t Press 1 for Hello World\n");
+    printf("\t Press 2 for Rotation Cipher\n");
+    printf("\t Press 3 for Substitution Cipher\n");
+
+    int userInput;
+    scanf("%d",&userInput); // Record User Input
+    printf("%d",userInput);
+    switch (userInput) {
+
+        case 1:
+            helloWorld();
+            break;
+
+        case 2:
+
+        case 3:
+
+
+        default:
+            printf("\nYou have Not Made A Selection");
+            break;
+    }
+}
+
 void helloWorld(){
+
+// Silly ACII Art printed to console, as displayed below
+    printf("+--------------------------------------------+\n");
+    printf("|  X                  X    X                 |\n");
+    printf("|  X                  X    X                 |\n");
+    printf("|  X                  X    X                 |\n");
+    printf("|  X                  X    X                 |\n");
+    printf("|  X                  X    X                 |\n");
+    printf("|  XXXXXX   XXXXXXXX  X    X      XXXX       |\n");
+    printf("|  XX   XX  XX     X  X    X    XX    XX     |\n");
+    printf("|  X     XX XXXXXXX   X    X   XX      XX    |\n");
+    printf("|  X      X X         X    X    XX    XX     |\n");
+    printf("|  X      X  XXXXX    X    X      XXXX       |\n");
+    printf("|                                            |\n");
+    printf("|                                            |\n");
+    printf("|  X         X                           XX  |\n");
+    printf("|  X        XXX             XX  X        XX  |\n");
+    printf("|  X   XX     X          XXX XX X        XXX |\n");
+    printf("|  X   XX     X  XXXXXX  X    X X         XX |\n");
+    printf("|  X XXXX    X  XX    XX X      X  XXXXX XXX |\n");
+    printf("|   X   X   XX  X    XX  X      X  XX  XXXXX |\n");
+    printf("|       XXXXX   XXXXX    X      X   XXX    X |\n");
+    printf("|                                            |\n");
+    printf("+--------------------------------------------+\n");
+
+
 /* Prints the text "Hello World" into the console*/
-  printf("Hello World");    
+  printf("\n\tHello World\n\n\n\n\n");
+  mainMenu();
 };
 
 
@@ -43,7 +126,7 @@ void encryptString(){
 	
 	// Collects and stores user entered characters
 	char textString[MAXCHAR];
-	char textInteger[MAXCHAR];
+	int textInteger[MAXCHAR];
 
 		// Initialise both arrays (FIX - External Function)
 		for(int index = 0; index<MAXCHAR; index++){
@@ -70,15 +153,16 @@ void encryptString(){
 	printf("%d\n",(int)textString[index]);  
 	
 	// Convert capitals letters into numbers A = 0, B = 1, C = 2... e.c.t.
-	if(textInteger[index]>=65||textInteger[index]<=90){ // Makes sure input is only capital letters
-		textInteger[index]=(int)textString[index]-65; 	// Converts string into textIntegerbers (and then makes them between 0 and 99)
-		printf("works\n"); \\FIX WAS JUST FOR TEST
+	if(textString[index]>=65&&textString[index]<=90){ // Makes sure input is only capital letters
+		textInteger[index]=(int)textString[index]-65; 	// Converts string into text Integers (and then makes them between 0 and 99)
+		printf("works\n"); //FIX WAS JUST FOR TEST
 	}else{
 	// Array entered doesn't include only capital letters
-		printf("ERROR: only enter in text");
+		printf("ERROR: only enter text in capital letters\n\n");
+        encryptString(); // Restarts Function
 	};
 	
-	printf("%d\n",textInteger[index]);
+	printf("%d\n",textInteger[index]); // prints current value of integer
 	break;
 /* 10 for quiz, 12.5 for assignment
 LOWERCASE STUFF FOR LATER
